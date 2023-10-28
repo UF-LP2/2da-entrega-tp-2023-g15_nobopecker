9a921392046e54c8136b875df81676843112d44b
agostina nobo <agosnobo@yahoo.com>
2023-10-28 15:14:30 -0300

    arbol!!!!!!!!!

--- library/bt_sintomas.py
+++ library/bt_sintomas.py
@@ -1,76 +1,73 @@
-from typing import Type
-
 from binarytree import Node
-from enums import eSintomas
-from enums import eEnfermedad
+
 def arbol_sintomas():
 
     #inicializo nodos
-    eSintomas.respiracion_irregular=Node(100)
-    eSintomas.pulsaciones_agitadas= Node(30)
-    eSintomas.dolor= Node(150)
-    eSintomas.nauseas= Node(15)
-    eSintomas.inconsciente= Node(50)
-    eEnfermedad.isquemia= Node(10)
-    eEnfermedad.convulsiones =Node(200)
-    eEnfermedad.coma = Node(40)
-    eEnfermedad.hemorragia_digestiva = Node(60)
-    eSintomas.mareo_desmayo = Node(120)
-    eSintomas.vision_borrosa = Node(110)
-    eEnfermedad.hipertension_arterial = Node(105)
-    eEnfermedad.vertigo = Node(115)
-    eSintomas.hinchazon = Node(140)
-    eSintomas.lagrimeo = Node(130)
-    eEnfermedad.cefalea_brusca = Node(125)
-    eEnfermedad.esguince = Node(135)
-    eSintomas.hemorragia = Node(145)
-    eEnfermedad.odontalgia = Node(142)
-    eEnfermedad.dolor_inespecifico_leve = Node(148)
-    eSintomas.aturdimiento= Node(180)
-    eSintomas.picazon = Node(160)
-    eEnfermedad.otalgia = Node(155)
-    eEnfermedad.sincope = Node(165)
-    eSintomas.ambulancia = Node(200)
-    eEnfermedad.politraumatismo_grave = Node(190)
-    eSintomas.urgencia_psiquiatrica = Node(220)
-    eEnfermedad.urgencia_psiquiatrica = Node(210)
-    eEnfermedad.no_urgencia = Node(230)
+    respiracion_irregular=Node(100)
+    pulsaciones_agitadas= Node(30)
+    dolor= Node(150)
+    nauseas= Node(15)
+    inconsciente= Node(50)
+    isquemia= Node(10)
+    convulsiones =Node(200)
+    coma = Node(40)
+    hemorragia_digestiva = Node(60)
+    mareo_desmayo = Node(120)
+    vision_borrosa = Node(110)
+    hipertension_arterial = Node(105)
+    vertigo = Node(115)
+    hinchazon = Node(140)
+    lagrimeo = Node(130)
+    cefalea_brusca = Node(125)
+    esguince = Node(135)
+    hemorragia = Node(145)
+    odontalgia = Node(142)
+    dolor_inespecifico_leve = Node(148)
+    aturdimiento= Node(180)
+    picazon = Node(160)
+    otalgia = Node(155)
+    sincope = Node(165)
+    ambulancia = Node(200)
+    politraumatismo_grave = Node(190)
+    urgencia_psiquiatrica_s = Node(220)
+    urgencia_psiquiatrica_e = Node(210)
+    no_urgencia = Node(230)
 
     #arbol
-    eSintomas.respiracion_irregular.left=eSintomas.pulsaciones_agitadas
-    eSintomas.pulsaciones_agitadas.left=eSintomas.nauseas
-    eSintomas.nauseas.left=eEnfermedad.isquemia
-    eSintomas.nauseas.right=eEnfermedad.convulsiones
-    eSintomas.pulsaciones_agitadas.right=eSintomas.inconsciente
-    eSintomas.inconsciente.left=eEnfermedad.coma
-    eSintomas.inconsciente.right=eEnfermedad.hemorragia_digestiva
+    respiracion_irregular.left=pulsaciones_agitadas
+    pulsaciones_agitadas.left=nauseas
+    nauseas.left=isquemia
+    nauseas.right=convulsiones
+    pulsaciones_agitadas.right=inconsciente
+    inconsciente.left=coma
+    inconsciente.right=hemorragia_digestiva
 
-    eSintomas.respiracion_irregular.right=eSintomas.dolor
-    eSintomas.dolor.left=eSintomas.mareo_desmayo
-    eSintomas.mareo_desmayo.left=eSintomas.pulsaciones_agitadas
-    eSintomas.vision_borrosa.left=eEnfermedad.hipertension_arterial
-    eSintomas.vision_borrosa.right=eEnfermedad.vertigo
+    respiracion_irregular.right=dolor
+    dolor.left=mareo_desmayo
+    mareo_desmayo.left=pulsaciones_agitadas
+    vision_borrosa.left=hipertension_arterial
+    vision_borrosa.right=vertigo
 
-    eSintomas.mareo_desmayo.right=eSintomas.hinchazon
-    eSintomas.hinchazon.left=eSintomas.lagrimeo
-    eSintomas.lagrimeo.left=eEnfermedad.cefalea_brusca
-    eSintomas.lagrimeo.right=eEnfermedad.esguince
-    eSintomas.hinchazon.right=eSintomas.hemorragia
-    eSintomas.hemorragia.left=eEnfermedad.odontalgia
-    eSintomas.hemorragia.right=eEnfermedad.dolor_inespecifico_leve
+    mareo_desmayo.right=hinchazon
+    hinchazon.left=lagrimeo
+    lagrimeo.left=cefalea_brusca
+    lagrimeo.right=esguince
+    hinchazon.right=hemorragia
+    hemorragia.left=odontalgia
+    hemorragia.right=dolor_inespecifico_leve
 
-    eSintomas.dolor.right=eSintomas.aturdimiento
-    eSintomas.aturdimiento.left=eSintomas.picazon
-    eSintomas.picazon.left=eEnfermedad.otalgia
-    eSintomas.picazon.right=eEnfermedad.sincope
+    dolor.right=aturdimiento
+    aturdimiento.left=picazon
+    picazon.left=otalgia
+    picazon.right=sincope
 
-    eSintomas.aturdimiento.right=eSintomas.ambulancia
-    eSintomas.ambulancia.left=eEnfermedad.politraumatismo_grave
-    eSintomas.ambulancia.right=eSintomas.urgencia_psiquiatrica
-    eSintomas.urgencia_psiquiatrica.left=eEnfermedad.urgencia_psiquiatrica
-    eSintomas.urgencia_psiquiatrica.right=eEnfermedad.no_urgencia
+    aturdimiento.right=ambulancia
+    ambulancia.left=politraumatismo_grave
+    ambulancia.right=urgencia_psiquiatrica_s
+    urgencia_psiquiatrica_s.left=urgencia_psiquiatrica_e
+    urgencia_psiquiatrica_s.right=no_urgencia
 
-    print (eSintomas.respiracion_irregular)
+    print (respiracion_irregular)
 
 if __name__ == "__main__":
     arbol_sintomas()
