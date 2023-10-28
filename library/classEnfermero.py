98f63a1b35e1e0f51e7493d9a892ebcca99be3e2
Paula Pecker <paulapeckerfasce@gmail.com>
2023-10-28 11:15:04 -0300

    librerias de nuevo

--- library/classEnfermero.py
+++ library/classEnfermero.py
@@ -13,4 +13,27 @@ class cEnfermero:
         #if arbol_sintomas.right==null
 
     def clasificar(paciente:cPaciente):
-            if paciente.diagnostico==politraumatismo_grave:
+            if paciente.diagnostico.color==rojo:
+                #atender_urgencia(paciente)
+            elif paciente.diagnostico==eSintomas_Enfermedades.coma||paciente.diagnostico==eSintomas_Enfermedades.convulsiones||
+
+
+####naranja
+    coma=24
+    convulsiones=25
+    hemorragia_digestiva=26
+    isquemia=27
+    #amarillo
+    cefalea_brusca=28
+    paresia=29
+    hipertension_arterial=30
+    vertigo=31
+    sincope=32
+    #urgencia_psiquiatrica=19
+    #verde
+    otalgia=33
+    odontalgia=34
+    dolor_inespecifico_leve=35
+    esguince=36
+    #azul
+    no_urgencia=37
--- library/classPaciente.py
+++ library/classPaciente.py
@@ -1,7 +1,9 @@
 from enums import eEstado
+from enums import eColor
+from enums import eSintomas_Enfermedades
 class cPaciente:
 
-    def __init__(self, ID, sintomas, diagnostico, estado=eEstado.enfermo):
+    def __init__(self, ID, sintomas:eSintomas_Enfermedades, diagnostico:eSintomas_Enfermedades, estado=eEstado.enfermo):
         self.ID=ID
         self.sintomas=sintomas
         self.diagnostico=diagnostico
