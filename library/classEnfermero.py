7babb98cb403c0d618d1c63dac9599c86fe3645b
Paula Pecker <paulapeckerfasce@gmail.com>
2023-10-27 21:10:54 -0300

    enfermero

--- library/classEnfermero.py
+++ library/classEnfermero.py
@@ -1,5 +1,12 @@
+
 class cEnfermero:
-    def __init__(self, ID, turno):
+
+    def __init__(self, ID, turno, arbol_sintomas):
         self.ID=ID
         self.turno=turno
+        arbol_sintomas=arbol_sintomas
+    def diagnosticar(paciente):
+        return definir_enfermedad(paciente.sintomas, arbol_sintomas)
+    def definir_enfermedad(sintomas, arbol_sintomas):
+        if arbol_sintomas.right==null
 
