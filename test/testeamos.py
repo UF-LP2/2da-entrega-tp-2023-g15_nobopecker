99c1a0153a04767ed71668f375fccfee96191043
Paula Pecker <paulapeckerfasce@gmail.com>
2023-11-06 18:02:56 -0300

    Merge branch 'main' of https://github.com/UF-LP2/2da-entrega-tp-2023-g15_nobopecker

--- test/testeamos.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-#sintaxis
-class Probando(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-class Probando2(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
